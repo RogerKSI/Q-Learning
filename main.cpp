a18354eab5f3f996b96742ab507ac399c602d170
RogerKSI <kittipong1996@hotmail.com>
2017-12-12 02:55:13 +0700

    config train

--- main.cpp
+++ main.cpp
@@ -7,157 +7,249 @@
 // Author is Jin Fagang.
 //
 //===----------------------------------------------------------------------===//
+
 #include <iostream>
-#include <string>
+#include <fstream>
 #include <string.h>
+#include <stdio.h>
 #include <time.h>
 #include "utils/utils.h"
 
+#include <opencv2/highgui.hpp>
+
 using namespace std;
+using namespace cv;
 
-// I think Q matrix and R matrix must be same
-// and it must be same both row and column
-#define MATRIX_ROW 6
-#define MATRIX_COLUMN 6
-// state num must be equal to action num
-#define STATE_NUM 6
-#define ACTION_NUM 6
-#define DES_STATE 5
-#define MAX_EPISODE 1000
-// this value using for Q(s,a)=R(s,a)+alpha * max{Q(s', a')}
-// Q(s', a') is the all next step Q value
-#define alpha 0.8
+#define MAP_HEIGHT 100
+#define SPACE_BETWEEN 100
+#define MAP_WIDTH 300
 
-// define the most important Q-matrix and R-matrix
-// R-matrix is given
-double R[100][100] = {{-1, -1, -1, -1, 0, -1},
-                     {-1, -1, -1, 0, -1, 100},
-                     {-1, -1, -1, 0, -1, -1},
-                     {-1, 0, 0, -1, 0, -1},
-                     {0, -1, -1, 0, -1, 100},
-                     {-1, 0, -1, -1, 0, 100}};
-double Q[100][100];
-int possible_action[10];
-int possible_action_num;
+#define ACTION_NUM 2
 
+#define MAX_EPISODE 100000
+#define ALPHA 0.8
 
-// get in state s, possible actions
-void get_possible_action(double R[100][100], int state, int possible_action[10]){
-    // find R[i][j] value > 0 is action we can do later
-    possible_action_num = 0;
-    for(int i = 0; i < ACTION_NUM; i++){
-        if (R[state][i] >= 0){
-            possible_action[possible_action_num] = i;
-            possible_action_num++;
-        }
-    }
+#define SHIFT_BIRD 10
+#define JUMP 2
+#define DOWN 1
+
+double R[MAP_HEIGHT][SPACE_BETWEEN][MAP_HEIGHT][ACTION_NUM];
+double Q[MAP_HEIGHT][SPACE_BETWEEN][MAP_HEIGHT][ACTION_NUM];
+
+vector<pair<int, int>> obstacles;
+
+bool cmp(const pair<int, int> &p1, const int v)
+{
+	if (p1.first<v)
+		return true;
+	else
+		return false;
 }
 
-double get_max_q(double Q[100][100], int state){
+vector<pair<int, int>>::iterator getNextObstacle(int t) {
+	return lower_bound(obstacles.begin(), obstacles.end(), t + SHIFT_BIRD, cmp);
+}
+
+void generateMap() {
+
+	obstacles.clear();
+	for (int i = SPACE_BETWEEN; i <= 10000000; i += SPACE_BETWEEN) {
+		obstacles.push_back({ i, (rand() % (MAP_HEIGHT - JUMP * 2)) + JUMP });
+	}
+}
+
+int playGame(int &position, int action, int t, bool draw = false) {
+
+	// update status of bird
+	if (action == 1)
+		position -= JUMP - DOWN;
+	else
+		position += DOWN;
+
+	auto obstacle = getNextObstacle(t);
+//	cout << "time : " << t << " | bird: (" << SHIFT_BIRD << " , " << position << ") => next obstacle (" << (obstacle->first - t) << " , " << (obstacle->second) << ")" << endl;
+	// draw game
+	if (draw) {
+
+	}
+
+	// check status of game (win / lose / nothing)
+	if (position <= 0 || position >= MAP_HEIGHT - 1)
+		return 1;
+
+	if (obstacle->first - t == SHIFT_BIRD) {
+		if (obstacle->second != position)
+			return 1;
+		else
+			return 2;
+	}
+	
+	return 0;
+}
+
+
+double get_max_q(int position, int action, int t, vector<pair<int,int>>::iterator obstacle){
     double temp_max = 0;
-    for (int i = 0; i < ACTION_NUM; ++i) {
-        if ((R[state][i] >= 0) && (Q[state][i] > temp_max)){
-            temp_max = Q[state][i];
+
+	int next_position;
+	if (action == 0)
+		next_position = position + DOWN;
+	else
+		next_position = position - JUMP + DOWN;
+
+	for (int i = 0; i < ACTION_NUM; ++i) {
+        if ((R[next_position][obstacle->first - t - 1][obstacle->second][i] >= 0) && (Q[next_position][obstacle->first - t - 1][obstacle->second][i] > temp_max)){
+			temp_max = Q[next_position][obstacle->first - t - 1][obstacle->second][i];
         }
     }
-    return temp_max;
+    
+	return temp_max;
 }
 
-int episode_iterator(int init_state, double Q[100][100], double R[100][100]){
+void episode_iterator(int position){
 
-    double Q_before, Q_after;
-    // next action
-    int next_action;
+	int action;
     double max_q;
 
     // start series event loop
-    int step=0;
-    while (1){
-        cout << "-- step " << step << ": initial state: " << init_state << endl;
-        // memset possible_action array
-        memset(possible_action, 0, 10*sizeof(int));
-        get_possible_action(R, init_state, possible_action);
+    int t = 0;
+    while (true){
 
-        // get next action
-        next_action = possible_action[rand()%possible_action_num];
-        cout << "-- step " << step << ": next action: " << next_action << endl;
-        // treat next action as state, and we can get max{Q(s', a')}
-        max_q = get_max_q(Q, next_action);
+		// get next action
+		if (position < JUMP)
+			action = 0;
+		else if (position > MAP_HEIGHT - JUMP)
+			action = 1;
+		else
+			action = rand() % 2;
 
-        Q_before = Q[init_state][next_action];
+		// treat next action as state, and we can get max{Q(s', a')}
+		auto obstacle = getNextObstacle(t);
+		max_q = get_max_q(position, action, t, obstacle);
+
         // update formula Q(s,a)=R(s,a)+alpha * max{Q(s', a')}
-        Q[init_state][next_action] = R[init_state][next_action] + alpha * max_q;
-        Q_after = Q[init_state][next_action];
+        Q[position][obstacle->first - t][obstacle->second][action] = R[position][obstacle->first - t][obstacle->second][action] + ALPHA * max_q;
 
-        // next episode rules
-        // if next_action is destination state, then go next episode
-        // if not, then go back to this loop
-        if (next_action == DES_STATE){
-            init_state = rand()%STATE_NUM;
-            break;
-        }else{
-            // if not destination state, then next action becomes initial state
-            init_state = next_action;
-        }
-        step++;
+		int result = playGame(position, action, t);
+		if (result == 1) {
+			break;
+		}
+
+		t++;
     }
-    return init_state;
+
 }
 
-int inference_best_action(int now_state, double Q[100][100]){
+int inference_best_action(int position, int t){
     // get the max value of Q corresponding action when state is nw_state
     double temp_max_q=0;
     int best_action=0;
+
+	auto obstacle = getNextObstacle(t);
+
     for (int i = 0; i < ACTION_NUM; ++i) {
-        if (Q[now_state][i] > temp_max_q){
-            temp_max_q = Q[now_state][i];
+        if (Q[position][obstacle->first - t][obstacle->second][i] > temp_max_q){
+			temp_max_q = Q[position][obstacle->first - t][obstacle->second][i];
             best_action = i;
         }
     }
     return best_action;
 }
 
-void run_training(int init_state){
-    int initial_state = init_state;
+void run_training(){
 
     // start random
-    srand((unsigned)time(NULL));
     cout << "[INFO] start training..." << endl;
     for (int i = 0; i < MAX_EPISODE; ++i) {
-        cout << "[INFO] Episode: " << i << endl;
-        initial_state = episode_iterator(initial_state, Q, R);
-        cout << "-- updated Q matrix: " << endl;
-        print_matrix(Q, 6, 6);
+		cout << "[INFO] Episode: " << i << endl;
+
+		int position = rand() % MAP_HEIGHT;
+		generateMap();
+
+		episode_iterator(position);
+
     }
 }
 
+void saveQMatrix() {
+	FILE *fp = fopen("./../QMatrix.txt", "w");
+	for (int i = 0; i < MAP_HEIGHT; i++) {
+		for (int j = 0; j < SPACE_BETWEEN; j++) {
+			for (int k = 0; k < MAP_HEIGHT; k++) {
+				fprintf(fp, "%lf %lf\n", Q[i][j][k][0], Q[i][j][k][1]);
+			}
+		}
+	}
+	fclose(fp);
+}
+
+void loadQMatrix() {
+	FILE *fp = fopen("./../QMatrix.txt", "r");
+	for (int i = 0; i < MAP_HEIGHT; i++) {
+		for (int j = 0; j < SPACE_BETWEEN; j++) {
+			for (int k = 0; k < MAP_HEIGHT; k++) {
+				fscanf(fp, "%lf %lf", &Q[i][j][k][0], &Q[i][j][k][1]);
+			}
+		}
+	}
+	fclose(fp);
+}
+
+
+void initialRMatrix() {
+	for (int i = 0; i < MAP_HEIGHT; i++) {
+		for (int j = 0; j < SPACE_BETWEEN; j++) {
+			for (int k = 0; k < MAP_HEIGHT; k++) {
+				if (i <= JUMP * 2)
+					R[i][j][k][1] = -100;
+				if (i > MAP_HEIGHT - JUMP * 2)
+					R[i][j][k][0] = -100;
+
+				if (j == SHIFT_BIRD - 1 && k == i - JUMP)
+					R[i][j][k][1] = 100;
+				if (j == SHIFT_BIRD - 1 && i + DOWN == k)
+					R[i][j][k][0] = 100;
+			}
+		}
+	}
+}
+
 int main() {
+	srand((unsigned)time(NULL));
 
-    cout << "Q matrix:" << endl;
-    print_matrix(Q, 6, 6);
-    cout << "R matrix:" << endl;
-    print_matrix(R, 6, 6);
+	cout << "initial" << endl;
+	initialRMatrix();
+	loadQMatrix();
 
-    run_training(1);
-    cout << "Q convergence matrix:" << endl;
-    print_matrix(Q, 6, 6);
+	cout << "train" << endl;
+	run_training();
+	saveQMatrix();
+	cout << "saved" << endl;
 
-    int position;
-    while (1){
-        cout << "please input robot locate room: " << endl;
-        cin >> position;
-        cout << position << "->";
-        while (1){
-            int best_action = inference_best_action(position, Q);
-            cout << best_action << "->";
-            if (best_action == DES_STATE){
-                cout << "out" << endl;
-                break;
-            }else{
-                position = best_action;
-            }
+	/*
+	Mat a = Mat(MAP_HEIGHT, MAP_WIDTH, CV_8UC3);
+	imshow("qq", a);
+
+	while (true){
+
+		int position = rand() % MAP_HEIGHT;
+		generateMap();
+		int t = 0;
+
+		while (true){
+            int best_action = inference_best_action(position, t);
+
+			int result = playGame(position, best_action, t);
+
+			if (result == 1) {
+				cout << "GG";
+				break;
+			}
+
+			t++;
+			waitKey(300);
         }
     }
-
+*/
     return 0;
 }
